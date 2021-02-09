07f0e2bc595a9185f0017a172acc984c21b83ae7
Viktor Ivanov <33068739+Nookiie@users.noreply.github.com>
2021-02-09 17:34:34 +0200

    Fixed Exercise 1 and added precision

--- lection5/ExamplePython/ExamplePython.py
+++ lection5/ExamplePython/ExamplePython.py
@@ -8,25 +8,22 @@ def print_exercise_header(n: str, isEnd: bool):
         print(f"\nEnd of Exercise {n}\n")
 
 ## Exercises
-
 print_exercise_header(1,False)
 
-#1. Напишете функция която приема 2 параметъра за катетите
+#1.  Напишете функция която приема 2 параметъра за катетите
 #на правоъгълен триъгълник и изчислява хипотенузата му.
 
 # Solution
 # -----------------------------------------------------------------------------------
+def pythagoras_function_print(a: float, b: float, precision: int = 3):
+    print(round(math.sqrt((a ** 2 + b ** 2)), precision))
 
-def pythagoras_function_print(a: int, b:int):
-       print(math.sqrt((a ** 2 + b ** 2)))
-
-pythagoras_function_print(2,5)
+pythagoras_function_print(28, 15)
 
 # -----------------------------------------------------------------------------------
-
 print_exercise_header(1,True)
 
-#2. Напишете функция която приема 1 параметър за броя на
+#2.  Напишете функция която приема 1 параметър за броя на
 #редовете в следната фигура и я отпечатва на конзолата.
 
 #*
@@ -36,12 +33,10 @@ print_exercise_header(1,True)
 #* * * * *
 
 #* * * * * * *
-
 print_exercise_header(2,False)
 
 # Solution
 # -----------------------------------------------------------------------------------
-
 def star_print(n: int):
     element = " * "
     spaces = " " * (n * 6)
@@ -49,7 +44,7 @@ def star_print(n: int):
     i = 1
     s = 1
 
-    for x in range(1, n + 1):
+    for x in range(0, n):
         stars = spaces + (element * i)
         print(stars[(i + s)::])
         i += 2
@@ -58,21 +53,18 @@ def star_print(n: int):
 star_print(8)
 
 # -----------------------------------------------------------------------------------
-
 print_exercise_header(2,True)
 
-#3. Напишете функция която отпечатва първите N на брой
+#3.  Напишете функция която отпечатва първите N на брой
 #елемента в редицата на Фибоначи.
-
 print_exercise_header(3,False)
 
 # Solution
 # -----------------------------------------------------------------------------------
-
 def fib_generator(n: int):
     if n < 0:
         return "Invalid Value"
-    elif n == 0:
+    if n == 0:
         return 0
     elif n == 1:
         return 1
@@ -89,5 +81,4 @@ def fib_print(n):
 fib_print(10)
 
 # -----------------------------------------------------------------------------------
-
 print_exercise_header(3,True)
