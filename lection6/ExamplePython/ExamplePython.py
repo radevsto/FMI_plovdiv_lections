77ba7a524214a3e84663d5a3c2cf15806a59a8f9
Viktor Ivanov <33068739+Nookiie@users.noreply.github.com>
2021-02-14 13:45:06 +0200

    CSV and JSON Output Task Refactoring and Retooling

--- lection6/ExamplePython/ExamplePython.py
+++ lection6/ExamplePython/ExamplePython.py
@@ -1,6 +1,7 @@
+import re
 import argparse
-import JsonConverter
-import CSVConverter
+from lib import JsonConverter
+from lib import CSVConverter
 
 ## Exercise
 
@@ -31,36 +32,34 @@ def get_dictionary_files(inputLines: str):
     if inputLines == None:
         return None
 
-    paragraphDict = {"paragraph": dict()}
-    paragraphs = []
+    paragraphsDict = {}
+    dictionaryFiles = {}
 
     for x in range(0, len(inputLines), 3):
-        if inputLines[x].startswith("## Begin"): 
-            paragraphDict = {((inputLines[x].partition("Begin ")[2]).replace('\n', '')).replace('P', 'p'): (inputLines[x + 1].replace('\n','').split(' '))}
+        if inputLines[x].startswith("## Begin"):
+            paragraphHeader = re.match('(Paragraph...?)', inputLines[x].partition("Begin ")[2])
+            inputLines[x+1] = "name=" + inputLines[x+1]
+            for i in range(0,len(inputLines[x+1].split(" "))):
+                dictionaryFiles[inputLines[x+1].replace("\n","").split(" ")[i].split("=")[0]] = inputLines[x+1].replace("\n","").split(" ")[i].split("=")[1]
 
-        paragraphs.append(paragraphDict)
-    
-    for paragraph in paragraphs:
-        for key in paragraph:
-            for _ in range(0, len(paragraph)):
-                paragraph[key][0] = "name=" + paragraph[key][0]
+            paragraphsDict[paragraphHeader.group(0).replace("P", "p")] = dictionaryFiles.copy()
+            
+    print(paragraphsDict)            
+    return paragraphsDict
 
-                for i in range(0, len(paragraph[key])):
-                    paragraph[key][i] = paragraph[key][i].split('=')
-                    paragraph[key][i] = {paragraph[key][i][0]:paragraph[key][i][1]}
-
-    return paragraphs
-
+# Conversion of Text File Content into JSON and CSV Files (Task #4)
 def main():
-    parser = argparse.ArgumentParser(description="Input Text File Path")
+    parser = argparse.ArgumentParser(description="A small application to convert a formatted Text File to a CSV or JSON file")
 
     parser.add_argument("--path", type = str,help = "Path for the file (By default, it's set as input.txt)")
     parser.add_argument("--format", type = str, help = "Output Input File to JSON, CSV or BOTH")
+    parser.add_argument("--output", type = str, help = "Name of the Output text file for CSV and JSON")
 
     args = parser.parse_args()
 
     file = args.path
     option = args.format
+    output = args.output
 
     if file is None:
         file = "input.txt"
@@ -68,17 +67,20 @@ def main():
     if option is None:
         option = "all"
     
+    if output is None:
+        output = "result"
+    
     while(1):
         if option.lower() == "csv":
-            CSVConverter.output_file(get_dictionary_files(get_text_file_input(file)))
+            CSVConverter.output_file(get_dictionary_files(get_text_file_input(file)), output)
             break
         elif option.lower() == "json":
-            JsonConverter.output_file(get_dictionary_files(get_text_file_input(file)))
+            JsonConverter.output_file(get_dictionary_files(get_text_file_input(file)), output)
             break
         elif option.lower() == "all":
             dictList = get_dictionary_files(get_text_file_input(file))
-            JsonConverter.output_file(dictList)
-            CSVConverter.output_file(dictList)
+            JsonConverter.output_file(dictList, output)
+            CSVConverter.output_file(dictList, output)
             break
         else:
             print("Unrecognised option, please try again")
