dfd1898169740990b68adc00b7a06dec4f623156
Anton Petkov <35229407+anton-petkov@users.noreply.github.com>
2021-02-17 18:36:08 +0200

    .

--- lection4/Nikoleta_Radeva/week 5/taskFirst.py
+++
@@ -1,8 +0,0 @@
-import math
-def calcilate_hypotenuse():
-    a = float(input('Please enter the side a: '))
-    b = float(input('Please enter the side b: '))
-    c = math.sqrt((a * a) + (b * b))
-    print("The hypotenuse side is: ", c)
-calcilate_hypotenuse()
-
--- lection4/Samir_Dermendzhiev/NumberCheck.py
+++
@@ -1,8 +0,0 @@
-x = range(0, 101)
-for n in x:
-    if n % 2 == 0:
-        print(n)
-
-# x = range(0, 101, 2)
-# for n in x:
-#     print(n)
