38d8107ab8920a614a618c5b5937962a79383323
Jordan <49492517+Xulww@users.noreply.github.com>
2021-02-18 21:40:11 +0200

    Update json_output.py

--- lection7/jordan_kondakov/json_output.py
+++ lection7/jordan_kondakov/json_output.py
@@ -3,6 +3,6 @@ import json
 
 
 def to_json(dict_structure):
-    json_file = open("json_formated_file.txt", "w+")
+    json_file = open("json_formated_file.json", "w+")
     json_file.write(json.dumps(dict_structure))
     json_file.close()
